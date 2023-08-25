a0503078bd9901d6f45e74554c57c5fcd0026816
Ivan Vostrilov <vnstr.main@gmail.com>
2023-08-25 16:52:29 +0300

    Init commit

---
+++ lua/core/mappings.lua
@@ -0,0 +1,23 @@
+vim.g.mapleader = " "
+
+-- NeoTree
+vim.keymap.set('n', '<leader>N', ':Neotree float focus <CR>')
+vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left <CR>')
+
+-- Tabby
+vim.api.nvim_set_keymap("n", "<leader>ta", ":$tabnew<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>tc", ":tabclose<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>to", ":tabonly<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>tn", ":tabn<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>tp", ":tabp<CR>", { noremap = true })
+-- move current tab to previous position
+vim.api.nvim_set_keymap("n", "<leader>tmp", ":-tabmove<CR>", { noremap = true })
+-- move current tab to next position
+vim.api.nvim_set_keymap("n", "<leader>tmn", ":+tabmove<CR>", { noremap = true })
+
+-- Formatting
+vim.keymap.set('n', '<leader>i', ':lua vim.lsp.buf.format() <CR>');
+
+-- LSP
+vim.keymap.set('n', '<leader>s', ':ClangdSwitchSourceHeader <CR>');
+
